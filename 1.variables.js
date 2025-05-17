fdc8b5e60ebe9cb411d0426af1bf91d2bf04122f
Thomas <przegralek.thomas@gmail.com>
2025-05-17 21:57:10 +0200

    Leçon : introduction aux variables

---
+++ 1.variables.js
@@ -0,0 +1,12 @@
+let prenom = "Marc";
+// Chaînes de caractères (string)
+
+let age = 21;
+// Nombre (number)
+
+let majorite = true;
+// Boolen (boolean)
+
+const nom="Thomas"
+
+console.log(prenom);
