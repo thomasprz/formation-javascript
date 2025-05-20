d25898fb3f4e0b6debc8c2c69ebcb2cbf68e223f
Thomas <przegralek.thomas@gmail.com>
2025-05-20 21:28:35 +0200

    Ajout d'exemples de fonctions anonymes

---
+++ 10.fonction-anonyme.js
@@ -0,0 +1,7 @@
+let fonctionAnonyme = function() {
+   console.log("Je suis une fonction anonyme")
+}
+
+fonctionAnonyme();
+
+(function() {console.log("Je suis une fonction anonyme auto-exécutée")})();
