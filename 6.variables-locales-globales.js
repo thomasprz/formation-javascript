8b381e2e32b00d1a49f12be183f7a586d72a42a5
Thomas <przegralek.thomas@gmail.com>
2025-05-20 21:28:21 +0200

    Ajout d'un fichier sur les variables locales et globales

---
+++ 6.variables-locales-globales.js
@@ -0,0 +1,8 @@
+let crie // Variable Globale
+function crieDeGuerre() {
+    crie = "Toujours plus fort !"
+    console.log(crie)
+}
+
+crieDeGuerre()
+console.log(crie) // Si la variable let crie avait été initialisé dans la fonction il y aurait eu un message d'erreur.
