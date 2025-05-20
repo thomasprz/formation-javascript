36171f8390e8aa6121b3ca92c6abedb8974ac096
Thomas <przegralek.thomas@gmail.com>
2025-05-20 21:28:13 +0200

    Ajout d'exercices sur les opérateurs en JavaScript

---
+++ 3.operateurs.js
@@ -0,0 +1,9 @@
+let nombreUn = 8;
+let nombreDeux = 4;
+let result
+
+result = nombreUn + nombreDeux
+result += 10
+result++ //incrementation = ajoute 1
+result-- //décrémentation = retirer 1
+console.log(result)
