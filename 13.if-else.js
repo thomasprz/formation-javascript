ac48b0af186d024ee861157362a7783bc4652b64
Thomas <przegralek.thomas@gmail.com>
2025-05-22 20:40:16 +0200

    Exercices if/else

---
+++ 13.if-else.js
@@ -0,0 +1,15 @@
+let age = 76;
+// heure == 11
+// heure != 11
+// heure < 11
+// heure > 11
+// heure <= 11
+// heure >= 11
+
+if (age < 18) {
+  console.log("Vous êtes mineur");
+} else if (age < 21) {
+  console.log("Vous êtes majeur en France.");
+} else {
+  console.log("Vous êtes majeur partout, à vous les casinos!");
+}
